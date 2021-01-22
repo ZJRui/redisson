0419bf7787ef10f5358cfa1b8f24d08025d68232
zjr <zhangjingrui@pekon.com>
2021-01-22 17:17:45 +0800

    commit

--- TestRedisson/src/main/java/com/sachin/RedissonTest.java
+++ TestRedisson/src/main/java/com/sachin/RedissonTest.java
@@ -57,4 +57,11 @@ public class RedissonTest implements Runnable {
             new Thread(new RedissonTest()).start();
         }
     }
+    public static void mainB(String[] args) {
+        init();
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(new RedissonTest()).start();
+        }
+    }
 }
